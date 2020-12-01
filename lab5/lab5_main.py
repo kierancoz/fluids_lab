d12f8c102e51b03eb613b75bf5e5f082bd3e0889
kierancoz <kierancoz.w@gmail.com>
2020-12-01 15:35:00 -0600

    pre camaro sting fix

--- lab5/lab5_main.py
+++ lab5/lab5_main.py
@@ -6,23 +6,21 @@ from math import sqrt
 def main():
     lab_data = json.load(open("lab_data.json","r+"))
 
-    # get calibration functions
-    v_to_p_small = v_to_p_func(lab_data.get("calibration_small"), plot = False)
-    v_to_p_big = v_to_p_func(lab_data.get("calibration_large"), plot = False)
-    v_to_drag_small = v_to_f_func(lab_data.get("calibration_small_drag"), plot = False)
-    v_to_drag_big = v_to_f_func(lab_data.get("calibration_large_drag"), plot = False)
-    v_to_lift_big = v_to_f_func(lab_data.get("calibration_large_lift"), plot = False)
 
     # Re = density * velocity * width / viscosity
     reynold_number = lambda velocity, width: 1.225 * velocity * width / (1.81 * 10**-5)
     # C_l/C_d = force / (area normal to flow * dynamic pressure)
     force_coefficient = lambda force, area, dyn_pres: force / (area * dyn_pres)
     # Pdyn = 1/2 * p * v^2
-    p_to_vel = lambda x: sqrt(x * 2 / 1.225)
+    p_to_vel = lambda x: sqrt(x * 2 / 1.225) if x > 0 else 0
+
 
     # # # # # # # # # # # # # #
     # # # 24" WIND TUNNEL # # #
     # # # # # # # # # # # # # #
+    v_to_p_big = v_to_p_func(lab_data.get("calibration_large"), plot = False)
+    v_to_drag_big = v_to_f_func(lab_data.get("calibration_large_drag"), plot = False)
+    v_to_lift_big = v_to_f_func(lab_data.get("calibration_large_lift"), plot = False)
 
     # get large sting information
     sting_drag, sting_lift = get_forces(lab_data.get("large_sting"), v_to_drag_big, v_to_lift_big)
@@ -44,9 +42,9 @@ def main():
     f150down_drag = np.array(f150down_total_drag) - np.array(sting_drag)
     f150down_lift = np.array(f150down_total_lift) - np.array(sting_lift)
 
-    # uncomment below for plots
-    plot_forces("Drag", sting_wind_velocities, plot = False, f1 = f1_drag, f150up = f150up_drag, f150down = f150down_drag)
-    plot_forces("Lift", sting_wind_velocities, plot = False, f1 = f1_lift, f150up = f150up_lift, f150down = f150down_lift)
+    # drag and lift plots for f1 and f150 in different configs
+    plot_forces("Drag", sting_wind_velocities, plot = False, f1 = f1_drag, f150_up = f150up_drag, f150_down = f150down_drag)
+    plot_forces("Lift", sting_wind_velocities, plot = False, f1 = f1_lift, f150_up = f150up_lift, f150_down = f150down_lift)
     
     # reynold numbers
     f1_rn = [reynold_number(vel, 0.1) for vel in sting_wind_velocities]
@@ -68,9 +66,40 @@ def main():
     plot_force_coeff("C_d", plot = False, f1 = {"x" : f1_rn, "y" : f1_c_d}, f150_down = {"x" : f150_rn, "y" : f150down_c_d},
         f150_up = {"x" : f150_rn, "y" : f150up_c_d})
 
+
     # # # # # # # # # # # # # #
     # # # 12" WIND TUNNEL # # #
     # # # # # # # # # # # # # #
+    v_to_p_small = v_to_p_func(lab_data.get("calibration_small"), plot = False)
+    v_to_drag_small = v_to_f_func(lab_data.get("calibration_small_drag"), plot = False)
+
+    # get small sting information
+    sting_drag = get_forces(lab_data.get("small_camaro_alone"), v_to_drag_small)
+    sting_lift = None # just so it doesnt accidentally get used anywhere, resetting this value
+    sting_pressure = get_pressure(lab_data.get("small_camaro_alone"), v_to_p_small)
+    sting_wind_velocities = [p_to_vel(x) for x in sting_pressure]
+
+    # get forces for camaro for various USPS positions
+    camaro_f_drag = get_forces(lab_data.get("small_usps_front"), v_to_drag_small)
+    camaro_b_drag = get_forces(lab_data.get("small_usps_behind"), v_to_drag_small)
+
+    # drag plots for camaro with different USPS positions
+    # plot NOT correct rn - need to account for Dstring using provided equation
+    plot_forces("Drag", sting_wind_velocities, plot = False, just_camaro = sting_drag, usps_front = camaro_f_drag, usps_behind = camaro_b_drag)
+
+    # reynolds number
+    camaro_rn = [reynold_number(vel, 0.0554) for vel in sting_wind_velocities]
+
+    # c_d calcs
+    justcamaro_c_d = [force_coefficient(x, 0.00206, sting_pressure[i]) for i, x in enumerate(sting_drag)]
+    uspsfront_c_d = [force_coefficient(x, 0.00206, sting_pressure[i]) for i, x in enumerate(camaro_f_drag)]
+    uspsbehind_c_d = [force_coefficient(x, 0.00206, sting_pressure[i]) for i, x in enumerate(camaro_b_drag)]
+    
+    # somethings wrong with this plot lol
+    plot_force_coeff("C_d", plot = True, just_camaro = {"x" : camaro_rn, "y" : justcamaro_c_d}, usps_front = {"x" : camaro_rn, "y" : uspsfront_c_d},
+        usps_behind = {"x" : camaro_rn, "y" : uspsbehind_c_d})
+
+
 
 def plot_force_coeff(ylabel, plot = True, **data):
     if plot:
